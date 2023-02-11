3aeebfdc1145a9bccaf373a8c55ac99cb9eeff1c
preston <prestonprince18@gmail.com>
2023-02-10 23:06:42 -0700

    finished linkedListFoind

---
+++ structy/LinkedLists/linkedListFind.js
@@ -0,0 +1,35 @@
+// rite a function, linkedListFind, that takes in the head of a linked list and a target value. 
+// The function should return a boolean indicating whether or not the linked list contains the target.
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+// a -> b -> c -> d
+
+const linkedListFind = (head, target) => {
+    let current = head;
+
+    while (current !== null) {
+        if (current.val === target) return true;
+
+        current = current.next
+    };
+
+    return false
+}
+
+console.log(linkedListFind(a, "c")); // true
+console.log(linkedListFind(a, "q")); // false
