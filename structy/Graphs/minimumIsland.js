6069ba0e71d932c968d136f0ed6dabb5b31fd21b
preston <prestonprince18@gmail.com>
2023-03-12 14:54:42 -0600

    structy - minimumIsland - JS

---
+++ structy/Graphs/minimumIsland.js
@@ -0,0 +1,45 @@
+function minimumIsland(grid) {
+    const visited = new Set();
+
+    let smallest = Infinity;
+    for (let r = 0; r < grid.length; r += 1) {
+        for (let c = 0; c < grid[0].length; c += 1) {
+            const size = exploreSize(grid, r, c, visited);
+            if (size > 0 && size < smallest) {
+                smallest = size;
+            }
+        }
+    };
+
+    return smallest
+};
+
+function exploreSize(grid, r, c, visited) {
+    const rInbounds = 0 <= r && r < grid.length;
+    const cInbounds = 0 <= c && c < grid[0].length;
+
+    if (!rInbounds || !cInbounds) return 0;
+    if (grid[r][c] === 'W') return 0;
+
+    const pos = `${r},${c}`;
+    if (visited.has(pos)) return 0;
+    visited.add(pos);
+
+    let size = 1;
+    size += exploreSize(grid, r+1, c, visited);
+    size += exploreSize(grid, r-1, c, visited);
+    size += exploreSize(grid, r, c+1, visited);
+    size += exploreSize(grid, r, c-1, visited);
+    return size;
+};
+
+const grid = [
+    ['W', 'L', 'W', 'W', 'W'],
+    ['W', 'L', 'W', 'W', 'W'],
+    ['W', 'W', 'W', 'L', 'W'],
+    ['W', 'W', 'L', 'L', 'W'],
+    ['L', 'W', 'W', 'L', 'L'],
+    ['L', 'L', 'W', 'W', 'W'],
+  ];
+  
+console.log(minimumIsland(grid)); // -> 2
